03e2faa6279bf5282e4d883e14b632a0b33703fd
ZHAIKE <ZHAIKEsir@163.com>
2022-03-07 15:40:49 +0800

    log4j按天存储日志

---
+++ README.md
@@ -0,0 +1,3 @@
+# Log4j
+#### 日志按照天去存放
+https://blog.csdn.net/csdnluolei/article/details/87696391
---
+++ src/main/java/com/example/log4j/Log4jApplication.java
@@ -0,0 +1,16 @@
+package com.example.log4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZHAIKE
+ */
+@SpringBootApplication
+public class Log4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Log4jApplication.class, args);
+    }
+
+}
